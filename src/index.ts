8e056a1b7e11c416f4bc4b3f5ea8c5278b881427
Tzu Huan Tai <tzu.huan.tai@gmail.com>
2025-02-19 20:06:48 -0800

    refactor: move interface to another file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { MqttClient } from './mqtt/mqtt-client';
-import { PiCamera, IPiCameraOptions } from './rtc/pi-camera';
+import { PiCamera } from './rtc/pi-camera';
+import { IPiCameraOptions } from './rtc/pi-camera.interface';
 
 export {
   PiCamera,
--- src/rtc/pi-camera.ts
+++ src/rtc/pi-camera.ts
@@ -1,8 +1,7 @@
-import { IMqttConnectionOptions, MqttClient } from '../mqtt/mqtt-client';
+import { MqttClient } from '../mqtt/mqtt-client';
 import {
   arrayBufferToBase64,
   arrayBufferToString,
-  CodecType,
   CommandType,
   generateUid,
   RtcMessage,
@@ -11,103 +10,8 @@ import {
 } from '../utils/rtc-tools';
 import { CameraOptionType, CameraOptionValue } from '../utils/camera-controls';
 import { addWatermarkToImage, addWatermarkToStream } from '../utils/watermark';
-
-const MQTT_SDP_TOPIC: string = "sdp";
-const MQTT_ICE_TOPIC: string = "ice";
-const DEFAULT_TIMEOUT: number = 10000;
-
-export interface IPiCameraOptions extends IMqttConnectionOptions {
-  stunUrls: string[];
-  turnUrl?: string;
-  turnUsername?: string;
-  turnPassword?: string;
-  timeout?: number;
-  datachannelOnly?: boolean;
-  isMicOn?: boolean;
-  isSpeakerOn?: boolean;
-  credits?: boolean;
-  codec?: CodecType;
-}
-
-interface IPiCamera {
-  // Events
-  /**
-   * Emitted when the WebRTC peer connection state changes.
-   *
-   * @param state - The new state of the RTCPeerConnection.
-   */
-  onConnectionState?: (state: RTCPeerConnectionState) => void;
-
-  /**
-   * Emitted when the data channel is successfully opened.
-   *
-   * @param dataChannel - The opened RTCDataChannel instance for data communication.
-   */
-  onDatachannel?: (dataChannel: RTCDataChannel) => void;
-
-  /**
-   * Emitted after calling the `snapshot()` method. This event emits a base64-encoded image 
-   * once all image packets are received from the server.
-   *
-   * @param base64 - The base64 string representing the captured image.
-   */
-  onSnapshot?: (base64: string) => void;
-
-  /**
-   * Emitted when the P2P connection cannot be established within the allotted time. 
-   * Automatically triggers the `terminate()` function.
-   */
-  onTimeout?: () => void;
-
-  // Methods
-  /**
-   * Attaches the remote media stream to the specified media element for playback.
-   *
-   * @param mediaElement - The HTML video element where the remote media stream will be rendered.
-   */
-  attach(mediaElement: HTMLVideoElement): void;
-
-  /**
-   * Start trying to establish the WebRTC connection.
-   */
-  connect(): void;
-
-  /**
-   * Terminates the WebRTC connection.
-   */
-  terminate(): void;
-
-  /**
-   * Retrieves the current connection status.
-   */
-  getStatus(): RTCPeerConnectionState;
-
-  /** 
-   * Sets the camera option, such as 3A or so.
-   * @param key Camera Option Type
-   * @param value Value of the Camera Option
-   */
-  setCameraOption(key: CameraOptionType, value: CameraOptionValue): void;
-
-  /**
-   * Requests a snapshot image from the server.
-   * 
-   * @param quality - The range from `0` to `100`, determines the image quality. The default value is `30`.
-   */
-  snapshot(quality?: number): void;
-
-  /**
-   * Toggles the **local** audio stream on or off. If an argument is provided, it will force the state to the specified value, otherwise, the current state will be toggled.
-   * @param enabled 
-   */
-  toggleMic(enabled?: boolean): void;
-
-  /**
-   * Toggles the **remote** audio stream on or off. If an argument is provided, it will force the state to the specified value, otherwise, the current state will be toggled.
-   * @param enabled
-   */
-  toggleSpeaker(enabled?: boolean): void;
-}
+import { IPiCamera, IPiCameraOptions } from './pi-camera.interface';
+import { DEFAULT_TIMEOUT, MQTT_ICE_TOPIC, MQTT_SDP_TOPIC } from '../constants';
 
 export class PiCamera implements IPiCamera {
   onConnectionState?: (state: RTCPeerConnectionState) => void;
