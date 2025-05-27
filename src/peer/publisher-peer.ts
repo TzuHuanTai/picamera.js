1e875582f1ed5c42045e65079ca040f59156b550
Tzu Huan Tai <tzu.huan.tai@gmail.com>
2025-05-27 04:14:44 -0700

    feat: support datachannel via sfu

--- src/peer/publisher-peer.ts
+++ src/peer/publisher-peer.ts
@@ -1,10 +1,55 @@
+import { DataPacket_Kind, UserPacket, DataPacket } from '@livekit/protocol';
+import { CmdMessage, CmdType } from "../rtc/cmd-message";
+// import { stringToArrayBuffer } from "../utils/rtc-tools";
 import { ChannelId, RtcPeer, RtcPeerConfig } from "./rtc-peer";
 
 export class PublisherPeer extends RtcPeer {
+
+  private pubLossyChannel: RTCDataChannel;
+  private pubReliableChannel: RTCDataChannel;
+
   constructor(config: RtcPeerConfig) {
     super(config);
-    super.createDataChannel(ChannelId.Reliable, { ordered: true });
-    super.createDataChannel(ChannelId.Lossy, { ordered: true, maxRetransmits: 0 });
+    this.pubReliableChannel = super.createDataChannel(ChannelId.Reliable, { ordered: true });
+    this.pubLossyChannel = super.createDataChannel(ChannelId.Lossy, { ordered: true, maxRetransmits: 0 });
+
+    this.pubReliableChannel.onopen = () => {
+      this.onDatachannel?.(ChannelId.Reliable);
+    }
+    this.pubLossyChannel.onopen = () => {
+      this.onDatachannel?.(ChannelId.Lossy);
+    }
+
     console.debug("PublisherPeer created.");
+  }
+
+  sendMessage = (msg: string) => {
+    const command = new CmdMessage(CmdType.CUSTOM, msg);
+
+    // const data = stringToArrayBuffer(command.ToString());
+    const data = new TextEncoder().encode(command.ToString());
+
+    const packet = new DataPacket({
+      kind: this.options.ipcMode === 'lossy' ? DataPacket_Kind.LOSSY : DataPacket_Kind.RELIABLE,
+      value: {
+        case: 'user',
+        value: new UserPacket({
+          payload: data,
+          topic: 'ipc_topic',
+        })
+      }
+    });
+
+    const encoded = packet.toBinary();
+
+    if (this.options.ipcMode === 'lossy') {
+      if (this.pubLossyChannel.readyState === 'open') {
+        this.pubLossyChannel.send(encoded);
+      }
+    } else {
+      if (this.pubReliableChannel.readyState === 'open') {
+        this.pubReliableChannel.send(encoded);
+      }
+    }
   }
 }
--- src/peer/subscriber-peer.ts
+++ src/peer/subscriber-peer.ts
@@ -1,8 +1,27 @@
-import { RtcPeer, RtcPeerConfig } from "./rtc-peer";
+import { DataPacket } from "@livekit/protocol";
+import { ChannelLabel, RtcPeer, RtcPeerConfig } from "./rtc-peer";
 
 export class SubscriberPeer extends RtcPeer {
   constructor(config: RtcPeerConfig) {
     super(config);
     console.debug("SubscriberPeer created.");
+  }
+
+  override async onDataChannelMessage(label: ChannelLabel, event: MessageEvent): Promise<void> {
+    let buffer: ArrayBuffer | undefined;
+    if (event.data instanceof ArrayBuffer) {
+      buffer = event.data;
+    } else if (event.data instanceof Blob) {
+      buffer = await event.data.arrayBuffer();
+    } else {
+      console.error('unsupported data type', event.data);
+      return;
+    }
+
+    const dp = DataPacket.fromBinary(new Uint8Array(buffer));
+
+    if (dp.value?.case === 'user') {
+      super.dispatchPayload(label, dp.value.value.payload);
+    }
   }
 }
